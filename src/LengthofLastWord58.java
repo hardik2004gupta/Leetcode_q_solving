0fdc1059def27f504edbf6a63d061b738ffb5ccc
Hardik Gupta <136774880+hardik2004gupta@users.noreply.github.com>
2025-09-13 11:57:26 +0530

    Update LengthofLastWord58.java

--- src/LengthofLastWord58.java
+++ src/LengthofLastWord58.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class LengthofLastWord58 {
+    public static void main(String args[]) {
+
+    }
+    public int lengthOfLastWord(String s) {
+
+        int count = 0;
+        for(int i=s.length()-1;i>=0;i--){
+            count++;
+            if(s.charAt(i-1)==' '){
+                break;
+            }
+        }
+
+        return count;
+    }
 }

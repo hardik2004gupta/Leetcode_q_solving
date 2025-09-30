c16b339ae8cbaabefce391d29233aa57f7fbd133
Hardik Gupta <136774880+hardik2004gupta@users.noreply.github.com>
2025-09-30 16:55:53 +0530

    commit

---
+++ src/SearchInsertPosition35.java
@@ -0,0 +1,15 @@
+public class SearchInsertPosition35 {
+    public static void main(String[] args) {
+
+    }
+    public static int searchInsert(int[] nums, int target) {
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == target){
+                return i;
+            } else if(target < nums[i]){
+                return i;
+            }
+        }
+        return nums.length;
+    }
+}
